c730550276a8dcf5a544d74f732a6aa01902f4e7
Sunnet4 <awolaja.sunday@gmail.com>
2023-08-16 14:38:45 -0700

    upto date

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
-- lists all genres of the show Dexter.
+-- lists all genres of the show Dexter.
 -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
